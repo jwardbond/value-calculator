ee2a056bf43a23c482e6170eb5659cf729c5b226
Jesse WB <JesseWB.C@gmail.com>
2019-03-02 14:47:18 -0800

    Inputs in the dynamic form now correctly change state

--- src/Form.jsx
+++ src/Form.jsx
@@ -2,54 +2,73 @@ import React from "react";
 
 //A modular form for adding or removing values
 class Form extends React.Component {
+  
+
+  //STATE------------------------------------------------------------------
   state = {
-    valueArr: ["", ""]
+    inputArr: [""]
   };
+
+  //EVENT HANDLERS----------------------------------------------------------
+
+  //the input fields are numbered, and when the value of the fields change
+  //this function will update the appropriate value in inputArr
+  handleInputChange = (e) => {
+    e.preventDefault();
+    let inputArr = [...this.state.inputArr];
+    inputArr[e.target.id] = e.target.value;
+    this.setState({inputArr})
+  }
 
   addLine = e => {
     e.preventDefault();
-    console.log("Pressed");
     this.setState(prevState => {
       return ({
-        valueArr: [...prevState.valueArr, ""]
+        inputArr: [...prevState.inputArr, ""]
       });
     });
   };
 
   removeLine = e => {
     e.preventDefault();
-    console.log("Pressed");
     this.setState(prevState => {
-      prevState.valueArr.pop(); //remove an element
+      prevState.inputArr.pop(); //remove an element
       return ({
-        valueArr: [...prevState.valueArr]
+        inputArr: [...prevState.inputArr]
       });
     });
   };
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("formsubmit");
+  }
+
+  //RENDER------------------------------------------------------------------------------------
   render() {
-    let { valueArr } = this.state;
+    let { inputArr } = this.state;
     let addLine = this.addLine;
     let removeLine = this.removeLine;
+    let handleInputChange = this.handleInputChange;
+    let handleSubmit = this.handleSubmit;
 
     return (
-      <form>
-        {//The dynamically added fields
-        valueArr.map((val, indexNo) => {
-          let fieldNo = indexNo + 1;
+      <form onChange={handleInputChange}>
+        {
+        inputArr.map((val, indexNum) => {
           return (
-            <div key={fieldNo}>
+            <div key={indexNum}>
               <br />
-              <label htmlFor={fieldNo}>Value #{fieldNo}</label>
+              <label htmlFor={indexNum}>Value #{indexNum+1}</label>
               <br />
-              <input type="text" />
+              <input id={indexNum} type="text" />
               <br />
             </div>
           );
         })}
-        <button onClick={addLine}>Add Line</button>
-        <button onClick={removeLine}>Remove Line</button>
-        <button>Submit</button>
+        <button onClick={addLine} type="button">Add Line</button>
+        <button onClick={removeLine} type="button">Remove Line</button>
+        <button onClick={handleSubmit}>Submit</button>
       </form>
     );
   }
