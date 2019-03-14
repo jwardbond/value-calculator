ad9317bee000ae40ca783b38ce6d9cd4fd670f82
Jesse WB <JesseWB.C@gmail.com>
2019-03-13 21:03:07 -0700

    Bug fix to remove incorrect line.

--- src/Questions.jsx
+++ src/Questions.jsx
@@ -1,11 +1,26 @@
-// import React from "react";
+import React from 'react';
 
-// class Questions extends React.component {
-//     state = {
-//         activeQuestions: ["", ""]
-//     }
+class Questions extends React.Component {
+    // state = {
+    //     /*set initial state*/
+    // }
 
+    // getDerivedStateFromProps(nextProps) {
+    //     /*if the props have changed
+    //         re-set questions
+    //     */
+    // }
 
-// }
+    render() {
+        let formContents = this.props.formContents;
+        return(
+            <div>
+                <h1>Questions</h1>
+                <h2>{formContents}</h2>
+            </div>
+        );
+    }
 
-// export default Questions;
+}
+
+export default Questions;
